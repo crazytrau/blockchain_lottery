a6ca586fb35f7c59367e3bcd380c5ad193be8640
crazytrau <htphuocdl@gmail.com>
2018-05-30 23:26:31 +0700

    getLastBlock, createNewTransaction

--- learning/dev/test.js
+++ learning/dev/test.js
@@ -3,8 +3,15 @@ const Blockchain = require('./blockchain');
 const bitcoin = new Blockchain();
 
 bitcoin.createNewBlock(125232,'7xhg3d7634grih23h','idb71dn71wndqsd');
-bitcoin.createNewBlock(123,'7xhg3d7634grih23h','idb71dn71wndqsd');
-bitcoin.createNewBlock(3151,'7xhg3d7634grih23h','idb71dn71wndqsd');
 
+bitcoin.createNewTransaction(523,'CTRasjdh81h1', 'BIGabs8bd11b1d');
+
+bitcoin.createNewBlock(18212,'aasdyb981dn1fasf','asjdb12d871b27d');
+
+bitcoin.createNewTransaction(1,'CTRasjdh81h1', 'BIGabs8bd11b1d');
+bitcoin.createNewTransaction(23,'CTRasjdh81h1', 'BIGabs8bd11b1d');
+bitcoin.createNewTransaction(53,'CTRasjdh81h1', 'BIGabs8bd11b1d');
+
+bitcoin.createNewBlock(11212,'ns8dny91nd1d','8n2h3y8d179db9');
 
 console.log(bitcoin);
