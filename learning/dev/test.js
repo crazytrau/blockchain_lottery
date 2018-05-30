6f02abbcd6947158c599d7e25609e4e371f5ace3
crazytrau <htphuocdl@gmail.com>
2018-05-31 00:10:44 +0700

    hashBlock

--- learning/dev/test.js
+++ learning/dev/test.js
@@ -2,16 +2,9 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(125232,'7xhg3d7634grih23h','idb71dn71wndqsd');
+const previousBlockHash = 'asdh781d81bdywqb91d';
+const currentBlockData = [  {amount:120,sender:'aiosu1dy1',recipient:'1d91yd71td8'},
+                            {amount:20,sender:'jd18d29',recipient:'28c7b2btc82w'}];
+const nonce = 981;
 
-bitcoin.createNewTransaction(523,'CTRasjdh81h1', 'BIGabs8bd11b1d');
-
-bitcoin.createNewBlock(18212,'aasdyb981dn1fasf','asjdb12d871b27d');
-
-bitcoin.createNewTransaction(1,'CTRasjdh81h1', 'BIGabs8bd11b1d');
-bitcoin.createNewTransaction(23,'CTRasjdh81h1', 'BIGabs8bd11b1d');
-bitcoin.createNewTransaction(53,'CTRasjdh81h1', 'BIGabs8bd11b1d');
-
-bitcoin.createNewBlock(11212,'ns8dny91nd1d','8n2h3y8d179db9');
-
-console.log(bitcoin);
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
